12bf237528f5be880fd9895ef642f485e77e30b6
Simone Anthony <s.anthony@tier1performance.com>
2024-11-21 21:27:03 -0500

    helmet implemented on backend for http header

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import cors from "cors";
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json());
+
+// Implement CORS
 app.use(cors());
+
+// Set security HTTP headers
+app.use(helmet());
