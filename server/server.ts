386a0dc431966797ffcbb124dde0ca89e273dc54
Simone Anthony <s.anthony@tier1performance.com>
2024-12-02 22:00:10 -0500

    nodemon and typescript set up corrected

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import app from './app';
 
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 
 // connect DB HERE
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
