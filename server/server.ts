481477b3212f87b39008b5d36a371e429019a915
Simone Anthony <s.anthony@tier1performance.com>
2024-12-13 20:41:19 -0500

    database connection code has been added to server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,37 @@
 import dotenv from "dotenv";
-import 'module-alias/register';
-import app from '@root/app';
+import "module-alias/register";
+import { pool } from "@db/index";
+import app from "@root/app";
 
+dotenv.config({ path: "./config.env" });
 
-dotenv.config({ path: './config.env' });
-
-// connect DB HERE
-
+// connect DB
+(async () => {
+  try {
+    await pool.connect();
+    console.log("DB initialized");
+  } catch (err) {
+    console.error('PG DB connection failed!', err)
+    process.exit(-1)
+  }
+})();
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+process.on("unhandledRejection", (err: Error) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
-process.on('unhandledRejection', (err: Error) => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
-    });
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
   });
-  
-process.on('SIGTERM', () => {
-    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-    server.close(() => {
-      console.log('💥 Process terminated!');
-    });
-  });
+});
