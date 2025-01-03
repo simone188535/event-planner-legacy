bbb58be7cd3083b9b22da91765b3a3aace20e02a
Simone Anthony <s.anthony@tier1performance.com>
2025-01-03 10:40:45 -0500

    Create Table users migration corrected

--- server/migrations/20241222023256_create_users_table.ts
+++ server/migrations/20241222023256_create_users_table.ts
@@ -1,10 +1,19 @@
 import type { Knex } from "knex";
+import catchMigration from "@utils/catchMigration";
+// import AppError from "@utils/appError";
 
+exports.up = catchMigration(async (knex: Knex): Promise<void> => {
+  await knex.raw(`
+            CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            first_name VARCHAR(50),
+            last_name VARCHAR(75),
+            username VARCHAR(50) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            date_created TIMESTAMP WITH TIME ZONE DEFAULT NOW())
+            `);
+});
 
-export async function up(knex: Knex): Promise<void> {
-}
-
-
-export async function down(knex: Knex): Promise<void> {
-}
-
+exports.down = catchMigration(async (knex: Knex): Promise<void> => {
+  await knex.raw(`DROP TABLE users`);
+});
