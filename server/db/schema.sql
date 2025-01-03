a91e344cbfad99049121afb75c0884eb4a2c4e28
Simone Anthony <s.anthony@tier1performance.com>
2025-01-03 10:28:35 -0500

    database name altered

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,8 +1,8 @@
 -- if this db already exists, delete it
-DROP DATABASE IF EXISTS event_planner_legacy_db;
+DROP DATABASE IF EXISTS event_planner_legacy;
 
 -- create a new db
-CREATE DATABASE event_planner_legacy_db;
+CREATE DATABASE event_planner_legacy;
 
 -- Move into the db
-\c event_planner_legacy_db;
+\c event_planner_legacy;
