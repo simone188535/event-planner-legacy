d7e5070936a318041b0a43c9e8140d5eab98a487
Simone Anthony <s.anthony@tier1performance.com>
2025-01-16 15:29:10 -0500

    signup controller creates new user

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,15 +1,26 @@
 import catchAsync from "@utils/catchAsync";
+import { query } from "@db/index";
 import { Request, Response, NextFunction } from "express";
 
 const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    const { firstName, lastName, username, email, password, passwordConfirm } =
+      req.body;
 
-    return res.json({
-        status: 'success',
-        user: 'mike'
-    })
+    // destructure object (to expose the rows object) and get first element of the array in the same step
+    const { rows: [newUser] } = await query(
+      `
+    INSERT INTO users(first_name, last_name, username, email, password, password_confirm) 
+    VALUES($1, $2, $3, $4, $5, $6)
+    RETURNING *`,
+      [firstName, lastName, username, email, password, passwordConfirm]
+    );
+
+    return res.status(201).json({
+      status: "success",
+      user: newUser,
+    });
   }
 );
-
 
 export { signup };
