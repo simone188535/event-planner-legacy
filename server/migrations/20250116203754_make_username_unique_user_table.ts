658da99525c4b01bbd51e3f8fa45bfe688b321f5
Simone Anthony <s.anthony@tier1performance.com>
2025-01-23 19:58:32 -0500

    migration created to add unique key on username

--- server/migrations/20250116203754_make_username_unique_user_table.ts
+++ server/migrations/20250116203754_make_username_unique_user_table.ts
@@ -3,12 +3,9 @@ import catchMigration from "@utils/catchMigration";
 
 
 export const up = catchMigration(async (knex: Knex): Promise<void> => {
-    await knex.raw(`ALTER TABLE users ADD UNIQUE (username)`);
+    await knex.raw(`ALTER TABLE users ADD UNIQUE (username);`);
   });
 
-
-
-  export const down = catchMigration(async (knex: Knex): Promise<void> => {
-    await knex.raw(`ALTER TABLE users DROP CONSTRAINT IF EXISTS users_username_key`);
-  });
-
+export const down = catchMigration(async (knex: Knex): Promise<void> => {
+  await knex.raw(`ALTER TABLE users DROP CONSTRAINT IF EXISTS users_username_key;`);
+});
