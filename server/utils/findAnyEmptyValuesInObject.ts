93104465f8d0bae683c89310c58a55d2cf5cacc2
Simone Anthony <s.anthony@tier1performance.com>
2025-02-17 14:47:59 -0500

    findAnyEmptyValuesInObject utility created, returns the  the first key value pair is null or equal to an empty string.

---
+++ server/utils/findAnyEmptyValuesInObject.ts
@@ -0,0 +1,16 @@
+/*
+ when given an object (like req.body), this helper method iterates over it and looks and returns the 
+ the first key value pair is null or equal to an empty string.
+ */
+
+const allReqBodyObject = (obj) => Object.entries(obj).map(([key, val]) => ({
+  key,
+  value: val,
+}));
+
+
+const anyEmptyObjectVals = allReqBodyObject.find(
+  ({ key, value }) => value === "" || value === null
+);
+
+return anyEmptyObject;
