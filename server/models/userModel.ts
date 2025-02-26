16939187dc200a9ecb7936c49bbd852ba29dbefa
Simone Anthony <s.anthony@tier1performance.com>
2025-02-26 15:34:17 -0500

    models folder created, users moved to models folder

---
+++ server/models/userModel.ts
@@ -0,0 +1,10 @@
+export interface UserModel {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+    dateCreated: Date;
+}
