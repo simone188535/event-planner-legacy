9a80cd5d16331150666191e22039c95d6a0e4814
Simone Anthony <s.anthony@tier1performance.com>
2025-02-26 17:13:16 -0500

    signup validator added

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signup } from "@controllers/authController";
+import { signupValidator } from "@validators/authValidators";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", signup);
+authRouter.post("/sign-up", signupValidator(), signup);
 
 // module.exports = {authRouter};
 export { authRouter };
