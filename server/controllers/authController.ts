0306b1b14035a50dceed879437e65ccdc36a76fe
Simone Anthony <s.anthony@tier1performance.com>
2025-05-09 15:47:40 -0400

    commented out code removed from signup controller, dtos renamed to be method specific

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,23 +1,18 @@
 import catchAsync from "@utils/catchAsync";
-import { signupDto } from "dtos/auth.dtos";
+import { signupResBodyDto, signupReqBodyDto } from "dtos/auth.dtos";
 import { query } from "@db/index";
 import { Request, Response, NextFunction } from "express";
 import AppError from '@utils/appError';
 
 
 const signup = catchAsync(
-  async (req: Request<{}, {}, signupDto, {}>, res: Response, next: NextFunction) => {
+  async (req: Request<{}, signupResBodyDto, signupReqBodyDto, {}>, res: Response, next: NextFunction) => {
     const { firstName, lastName, username, email, password, passwordConfirm } =
       req.body;
 
       // if passwords do not match
       if (password !== passwordConfirm) {
         return next(new AppError(`password and confirm password need to match!`, 406));
-
-        console.log('hi');
-        return res.status(201).json({
-          status: "success",
-        });
       }
 
     // destructure object (to expose the rows object) and get first element of the array in the same step
