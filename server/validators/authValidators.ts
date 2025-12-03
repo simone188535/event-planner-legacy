da6812ac4885a79eb69e0591303bc466df719a68
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-02 21:56:14 -0500

    signupValidator passwordConfirm, password and email now checked for notEmpty

--- server/validators/authValidators.ts
+++ server/validators/authValidators.ts
@@ -1,17 +1,30 @@
 import { body } from "express-validator";
 
-export const signupValidator = () => [
-  body("firstName").notEmpty().withMessage("First name is required"),
-  body("lastName").notEmpty().withMessage("Last name is required"),
-  body("username").notEmpty().withMessage("Username is required"),
-  body("email").isEmail().withMessage("Email is invalid"),
+const signupValidator = () => [
+  body("firstName").notEmpty().withMessage("First name is required!"),
+  body("lastName").notEmpty().withMessage("Last name is required!"),
+  body("username").notEmpty().withMessage("Username is required!"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required!")
+    .isEmail()
+    .withMessage("Email is invalid!"),
   body("password")
+    .notEmpty()
+    .withMessage("password is required!")
     .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters"),
-  body("passwordConfirm").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Passwords do not match");
-    }
-    return true;
-  }),
+    .withMessage("Password must be at least 6 characters!"),
+  body("passwordConfirm")
+    .notEmpty()
+    .withMessage("Confirm password is required!")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
 ];
+
+const loginValidator = () => [body("email"), body("password")];
+
+export { signupValidator, loginValidator };
