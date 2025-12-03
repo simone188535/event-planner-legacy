01787e86c7360eaf85f53af5db912d0c33b30963
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-03 16:03:27 -0500

    checkValidator exported

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { signup, login } from "@controllers/authController";
 import { signupValidator, loginValidator } from "@validators/authValidators";
-import { checkValidators } from "@middlewares/validators";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", signupValidator(), checkValidators, signup);
+authRouter.post("/sign-up", signupValidator(), signup);
 
-authRouter.get("/login", loginValidator(), checkValidators, login);
+authRouter.get("/login", loginValidator(), login);
 
 export { authRouter };
