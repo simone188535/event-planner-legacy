7901c0a3a5ea07be6dafe68e64683e4a77f3c081
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-09 14:44:20 -0500

    checkValidators cleanup complete

--- server/middlewares/validators.ts
+++ server/middlewares/validators.ts
@@ -1,20 +1,11 @@
 import { Request, Response, NextFunction } from "express";
-import { validationResult, ValidationChain } from "express-validator";
+import { validationResult } from "express-validator";
 import AppError from "@utils/appError";
 
 const checkValidators =
   (req: Request, res: Response, next: NextFunction) => {
     const results = validationResult(req);
     const errors = results.array();
-
-    // const formattedResults = results.formatWith(error => error.msg as string);
-    console.log('helo validations', errors);
-    // if there are any errors...
-    // if (!errors.isEmpty()) {
-    //     console.log(errors.array());
-
-    // res.status(406).json({err: errors.array()});
-    // }
 
     // if there is one error, pass a single object to the AppError
     if (errors.length === 1) {
