1610aa407261cc0b4d9f633c5802e2039e3aac34
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-09 14:35:17 -0500

    checkValidators returns single and multiple errors to AppError class, multiple error are now sent to an array

--- server/middlewares/validators.ts
+++ server/middlewares/validators.ts
@@ -16,14 +16,27 @@ import AppError from "@utils/appError";
 //   };
 const checkValidators =
   (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    console.log('helo validations', errors.isEmpty());
-    // if there are any errors...
-    if (!errors.isEmpty()) {
-        console.log(errors.array());
+    const results = validationResult(req);
+    const errors = results.array();
 
-    res.status(406).json({err: errors.array()});
+    // const formattedResults = results.formatWith(error => error.msg as string);
+    console.log('helo validations', errors);
+    // if there are any errors...
+    // if (!errors.isEmpty()) {
+    //     console.log(errors.array());
+
+    // res.status(406).json({err: errors.array()});
+    // }
+
+    // if there is one error, pass a single object to the AppError
+    if (errors.length === 1) {
+       next(new AppError(errors[0].msg, 404));
+    } // if there is more than one error message
+    else if (errors.length > 1) {
+       next(new AppError("Multiple validation errors occurred.", 404, errors));
     }
+
+    // if there are multiple, pass multiple errors the AppError cause
 
     return next();
   };
