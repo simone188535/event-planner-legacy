5e9dc768c864248f868c3b1f22932505931f2999
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-11 13:51:45 -0500

    406 code added to validators

--- server/middlewares/validators.ts
+++ server/middlewares/validators.ts
@@ -9,11 +9,11 @@ const checkValidators =
 
     // if there is one error, pass a single object to the AppError
     if (errors.length === 1) {
-       next(new AppError(errors[0].msg, 404));
+       next(new AppError(errors[0].msg, 406));
     } // if there is more than one error message
     else if (errors.length > 1) {
        // if there are multiple, pass multiple errors the AppError array
-       next(new AppError("Multiple validation errors occurred.", 404, errors));
+       next(new AppError("Multiple validation errors occurred.", 406, errors));
     }
 
     return next();
