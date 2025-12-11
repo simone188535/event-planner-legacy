9e921e5cd856958bb4b494e69cdacedee0e617d8
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-11 11:45:49 -0500

    remove confirm password down command added

---
+++ server/migrations/20251210211253_remove_confirm_password.ts
@@ -0,0 +1,12 @@
+import type { Knex } from "knex";
+import catchMigration from "@utils/catchMigration";
+
+export const up = catchMigration(async (knex: Knex): Promise<void> => {
+    await knex.raw(``);
+  });
+
+export const down = catchMigration(async (knex: Knex): Promise<void> => {
+  await knex.raw(`ALTER TABLE users ADD COLUMN IF NOT EXISTS password_confirm VARCHAR(50) NOT NULL;`);
+});
+
+
