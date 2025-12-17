947b501fd3953f41136088f7eaa66e1b25ad710c
Simone Anthony <simone.anthony1@yahoo.com>
2025-12-17 15:15:39 -0500

    event Routes is now a protected route

--- server/routes/eventsRoutes.ts
+++ server/routes/eventsRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { protectValidator } from "@validators/authValidators";
+import {protect } from "@controllers/authController";
 import { addEvent } from "@controllers/eventsController";
 
 
 const eventsRouter = Router();
 
- eventsRouter.post("/add-event", protectValidator(), addEvent);
+eventsRouter.use(protectValidator(), protect);
+
+eventsRouter.post("/add-event", addEvent);
  //  eventsRouter.post("/add-event", addEventValidator(), addEvent);
 
 
