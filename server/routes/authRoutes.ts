5ba33fafe6cefbca7d2f8a95bdfa3a777b5cfcbd
Simone Anthony <simone.anthony1@yahoo.com>
2026-01-01 14:05:30 -0500

    forgotPassword added to authRoutes

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { signup, login } from "@controllers/authController";
+import { signup, login, forgotPassword } from "@controllers/authController";
 import { signupValidator, loginValidator } from "@validators/authValidators";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signupValidator(), signup);
 authRouter.post("/login", loginValidator(), login);
+
+authRouter.post("/forgot-password", forgotPassword);
+// authRouter.post("/reset-password", resetPassword);
 
 export { authRouter };
