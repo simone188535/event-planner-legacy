d5b57389473e64dcfd7aa12c16a4925faf28749e
Simone Anthony <simone.anthony1@yahoo.com>
2026-01-01 15:39:45 -0500

    forgot and reset password removed from authRoutes

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { signup, login, forgotPassword } from "@controllers/authController";
+import { signup, login } from "@controllers/authController";
 import { signupValidator, loginValidator } from "@validators/authValidators";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signupValidator(), signup);
 authRouter.post("/login", loginValidator(), login);
-
-authRouter.post("/forgot-password", forgotPassword);
-// authRouter.post("/reset-password", resetPassword);
 
 export { authRouter };
