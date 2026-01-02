48753c63d3780f645c57b129abb307302da70512
Simone Anthony <simone.anthony1@yahoo.com>
2026-01-02 11:15:56 -0500

    sendEmail ulitipy created

---
+++ server/utils/email.ts
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+import type { SendMailOptions } from 'nodemailer';
+import type { Options as SMTPTransportOptions } from 'nodemailer/lib/smtp-transport';
+
+
+const sendEmail = async (options: SendMailOptions) => {
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        },
+
+    }as SMTPTransportOptions);
+
+    // Define the email options
+    const mailOptions = {
+        from: 'Simone Anthony <simone.anthony1@gmail.com>',
+        to: options.to,
+        subject: options.subject,
+        text: options.text,
+        // html: 
+    }
+
+    // actually send the email
+    await transporter.sendMail(mailOptions);
+}
+
+export { sendEmail };
