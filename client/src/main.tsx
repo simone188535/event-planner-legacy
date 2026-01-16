285d33effd7e15e82a6cd7d77a66e17bb0faf1a1
Simone Anthony <simone.anthony1@yahoo.com>
2026-01-16 17:13:11 -0500

    Mui provider removed from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,32 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
-declare module "@mui/material/styles" {
-  interface Palette {
-    customColors: Record<string, string>;
-  }
-
-  interface PaletteOptions {
-    customColors?: Record<string, string>;
-  }
-}
-
-const theme = createTheme({
-  palette: {
-    customColors: {
-    },
-  }
-});
+import MuiProvider from './providers/MUI/index.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <ThemeProvider theme={theme}>
+     <MuiProvider>
     <App />
-    </ThemeProvider>
+   </MuiProvider>
   </StrictMode>,
 )
