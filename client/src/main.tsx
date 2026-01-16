25e1b7e9fab0b998eb88ee9996c4c9bd7d8fa959
Simone Anthony <simone.anthony1@yahoo.com>
2026-01-16 11:41:42 -0500

    Theme Provider created for MUI, customColors added to pallete for global use

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    customColors: Record<string, string>;
+  }
+
+  interface PaletteOptions {
+    customColors?: Record<string, string>;
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    customColors: {
+    },
+  }
+});
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+     <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
