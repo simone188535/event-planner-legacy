e63ede027d9f029304951b2d18028c340a6f9c97
Simone Anthony <simone.anthony1@yahoo.com>
2026-03-23 11:40:38 -0400

    Routes nor has login accounting for all public pages, authed user pages and non-authed user pages

--- client/src/providers/Routes/index.tsx
+++ client/src/providers/Routes/index.tsx
@@ -1,27 +1,63 @@
-import  { type FC } from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { type FC } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useAuth } from "../../context/Auth";
+import ProtectedRoute from "./ProtectedRoute";
 
 import Login from "../../pages/Login";
 import Signup from "../../pages/Signup";
 import Events from "../../pages/Events";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Events />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-]);
 
 const Routes: FC = () => {
-   return <RouterProvider router={router} />
-}
+  const { token } = useAuth();
+
+  // all users, logged in or not makes no difference
+  const routesForPublic = [
+    {},
+    //   {
+    //   path: "/about-us",
+    //   element: <div>About Us</div>,
+    // },
+  ];
+
+  // users who are logged in
+  const routesForAuthenticatedOnly = [
+    {
+      path: "/",
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: "/",
+          element: <Events />,
+        },
+        {
+          path: "/logout",
+          element: <div>Logout</div>,
+        },
+      ],
+    },
+  ];
+
+
+  // users who are not logged in
+  const routesForNotAuthenticatedOnly = [
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ];
+
+  const router = createBrowserRouter([
+  ...routesForPublic,
+  ...(!token ? routesForNotAuthenticatedOnly : []),
+  ...routesForAuthenticatedOnly,
+]);
+
+  return <RouterProvider router={router} />;
+};
 
 export default Routes;
