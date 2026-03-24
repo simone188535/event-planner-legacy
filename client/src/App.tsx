9214807539fd04a76c2caf7036c126d6faeb3da7
Simone Anthony <simone.anthony1@yahoo.com>
2026-03-24 15:46:59 -0400

    authprovider added to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
 import { Container } from "@mui/material";
 
 import Routes from "./providers/Routes";
+import AuthProvider from "./providers/Auth";
 
 export default function App() {
   return (
     <>
-      {/* Header here */}
-      <Container
-        sx={{ bgcolor: "customColors.whiteLilac", minHeight: '100vh' }}
-        maxWidth={false}
-        disableGutters
-        component="main"
-      >
-        <Routes />
-      </Container>
-      {/* Footer here */}
+      <AuthProvider>
+        {/* Header here */}
+        <Container
+          sx={{ bgcolor: "customColors.whiteLilac", minHeight: "100vh" }}
+          maxWidth={false}
+          disableGutters
+          component="main"
+        >
+          <Routes />
+        </Container>
+        {/* Footer here */}
+      </AuthProvider>
     </>
   );
 }
